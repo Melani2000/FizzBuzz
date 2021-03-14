bb6782df01af3929c8b9cddfaa85f98c7b04cc88
Melani2000 <damprathibha@gmail.com>
2021-03-14 14:21:09 +0530

    Developed with while loop. Then the program does not end until the user enters zero.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int number = 1;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Number : ");
-        int number = scanner.nextInt();
-        if (number%5==0 && number%3==0)
-            System.out.println("FizzBuzz");
-        else if (number%3 == 0)
-            System.out.println("Buzz");
-        else if (number%5 == 0)
-            System.out.println("Fizz");
-        else
-            System.out.println(number);
+        System.out.println("Enter 0 for exit the program");
+        while (number!=0) {
+            System.out.print("Number : ");
+            number = scanner.nextInt();
+            if (number == 0)
+                break;
+            if (number % 5 == 0 && number % 3 == 0)
+                System.out.println("FizzBuzz");
+            else if (number % 3 == 0)
+                System.out.println("Buzz");
+            else if (number % 5 == 0)
+                System.out.println("Fizz");
+            else
+                System.out.println(number);
+        }
 
     }
 }
